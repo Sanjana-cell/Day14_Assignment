e4578c58f812110f4224764b25946cfc30f940bd
Sanjana K R <sanjanakr97@gmail.com>
2020-07-07 15:41:19 +0530

    Added Code for mobile number validation

--- validation.sh
+++ validation.sh
@@ -1,14 +1,21 @@
-#!/bin/bash -x
+#!/bin/bash 
+
 #variables
 result_1=0
 result_2=0
 result_3=0
+result_4=0
+pattern_1=""
+pattern_1=""
+pattern_2=""
+pattern_3=""
+
 
 function nameValidation()
 {
 	name=$1
-	pattern="^[A-Z]{1}[A-Za-z]{2,}$"
-	if [[ $name =~ $pattern ]]
+	pattern_1="^[A-Z]{1}[A-Za-z]{2,}$"
+	if [[ $name =~ $pattern_1 ]]
 	then
 		echo 1
 	else
@@ -18,33 +25,59 @@ function nameValidation()
 
 function emailValidation(){
 	email=$1
-	pattern_1="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]*\@+b(l)\.co(.in)*$"
-	if [[ $email =~ $pattern_1 ]]
+	pattern_2="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]*\@+b(l)\.co(.in)*$"
+	if [[ $email =~ $pattern_2 ]]
 	then
 		echo 1
 	else
 		echo 0
 	fi
 }
-
+function mobileNoValidation(){
+	mobile=$1$2
+	pattern_3="^[\+0-9]{2,3}[ ]?[0-9]{10}$"
+	if [[ $mobile =~ $pattern_3 ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+echo "FIRST NAME VALIDATION"
+echo "---------------------"
 read -p "Enter First name " firstName
 result_1="$(nameValidation $firstName)"
 case $result_1 in
         1) echo "First Name is Valid";;
         0) echo "First Name is not Valid";;
 esac
+echo "____________________"
 
+echo "LAST NAME VALIDATION"
+echo "--------------------"
 read -p "Enter Last name " lastName
 result_2="$(nameValidation $lastName)"
 case $result_2 in
         1) echo "Last Name is Valid";;
         0) echo "Last Name is not Valid";;
 esac
+echo "_____________________"
 
+echo "EMAIL ID VALIDATION"
+echo "--------------------"
 read -p "Enter Email ID " email
 result_3="$(emailValidation $email)"
 case $result_3 in
         1) echo "Email id is Valid";;
         0) echo "Email id is not Valid";;
 esac
+echo "____________________"
 
+echo "MOBILE NUMBER VALIDATION"
+echo "------------------------"
+read -p "Enter mobile Number " num
+result_4="$(mobileNoValidation $num)"
+case $result_4 in
+	1) echo "Mobile Number is valid";;
+	0) echo "Mobile Number is not valid";;
+esac
