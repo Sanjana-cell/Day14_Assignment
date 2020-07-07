6f43e4743a0ae19d2101356a08357e62d114c8ad
Sanjana K R <sanjanakr97@gmail.com>
2020-07-07 12:11:12 +0530

    Added Code to validate last name

--- validation.sh
+++ validation.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
-read -p "Enter the first name " firstName
-pattern="^[A-Z][a-z]*{3,}$"
-if [[ $firstName =~ $pattern ]]
-then
-	echo "Valid First Name"
-else
-	echo "Not Valid First Name"
-fi
+function nameValidation()
+{
+	name=$1
+	pattern="^[A-Z]{1}[A-Za-z]{2,}$"
+	if [[ $name =~ $pattern ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+read -p "Enter First name" firstName
+result_1="$(nameValidation $firstName)"
+echo $result_1
+case $result_1 in
+        1) echo "First Name is Valid";;
+        0) echo "First Name is not Valid";;
+esac
 
+read -p "Enter First name" lastName
+result_2="$(nameValidation $lastName)"
+case $result_2 in
+        1) echo "Last Name is Valid";;
+        0) echo "Last Name is not Valid";;
+esac
