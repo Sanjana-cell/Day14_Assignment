46ab6eb80673e955e7653cb566d3b0a13471966a
Sanjana K R <sanjanakr97@gmail.com>
2020-07-07 16:04:52 +0530

    Added Code  for password validation with rule 1 i.e password should contain minimum 8 Characters

--- validation.sh
+++ validation.sh
@@ -1,21 +1,25 @@
 #!/bin/bash 
 
 #variables
+firstName=""
+lastName=""
+email=""
+num=0
+password=""
 result_1=0
 result_2=0
 result_3=0
 result_4=0
+result_5=0
 pattern_1=""
 pattern_1=""
 pattern_2=""
 pattern_3=""
-
-
+pattern_4=""
 function nameValidation()
 {
-	name=$1
 	pattern_1="^[A-Z]{1}[A-Za-z]{2,}$"
-	if [[ $name =~ $pattern_1 ]]
+	if [[ $1 =~ $pattern_1 ]]
 	then
 		echo 1
 	else
@@ -24,9 +28,8 @@ function nameValidation()
 }
 
 function emailValidation(){
-	email=$1
 	pattern_2="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]*\@+b(l)\.co(.in)*$"
-	if [[ $email =~ $pattern_2 ]]
+	if [[ $1 =~ $pattern_2 ]]
 	then
 		echo 1
 	else
@@ -34,9 +37,17 @@ function emailValidation(){
 	fi
 }
 function mobileNoValidation(){
-	mobile=$1$2
 	pattern_3="^[\+0-9]{2,3}[ ]?[0-9]{10}$"
-	if [[ $mobile =~ $pattern_3 ]]
+	if [[ $1$2 =~ $pattern_3 ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+function passwordValidation(){
+	pattern_4="^[a-zA-Z0-9@#$&+-.$%^&*(),;\"\'?/]{8,}$"
+	if [[ $1 =~ $pattern_4 ]]
 	then
 		echo 1
 	else
@@ -81,3 +92,13 @@ case $result_4 in
 	1) echo "Mobile Number is valid";;
 	0) echo "Mobile Number is not valid";;
 esac
+
+echo "PASSWORD VALIDATION"
+echo "------------------------"
+read -p "Enter Password " password
+result_5="$(passwordValidation $password)"
+case $result_5 in
+	1) echo "Password is valid";;
+	0) echo "Password is not valid";;
+esac
+echo "____________________"
