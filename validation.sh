4f11dbdc5e2a5c6c50883b464bf865913db73444
Sanjana K R <sanjanakr97@gmail.com>
2020-07-07 12:49:12 +0530

    Added Code for email validation

--- validation.sh
+++ validation.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -x
+#variables
+result_1=0
+result_2=0
+result_3=0
+
 function nameValidation()
 {
 	name=$1
@@ -10,17 +15,36 @@ function nameValidation()
 		echo 0
 	fi
 }
-read -p "Enter First name" firstName
+
+function emailValidation(){
+	email=$1
+	pattern_1="^[a-zA-Z][0-9a-zA-Z\_\.\&\+\#]*\@+b(l)\.co(.in)*$"
+	if [[ $email =~ $pattern_1 ]]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+}
+
+read -p "Enter First name " firstName
 result_1="$(nameValidation $firstName)"
-echo $result_1
 case $result_1 in
         1) echo "First Name is Valid";;
         0) echo "First Name is not Valid";;
 esac
 
-read -p "Enter First name" lastName
+read -p "Enter Last name " lastName
 result_2="$(nameValidation $lastName)"
 case $result_2 in
         1) echo "Last Name is Valid";;
         0) echo "Last Name is not Valid";;
 esac
+
+read -p "Enter Email ID " email
+result_3="$(emailValidation $email)"
+case $result_3 in
+        1) echo "Email id is Valid";;
+        0) echo "Email id is not Valid";;
+esac
+
